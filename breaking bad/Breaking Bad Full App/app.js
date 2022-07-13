62feb6da67ecda310517ba9e211492ae8fa8a9b2
jd5624 <johnny.dove@hotmail.com>
2022-07-14 00:37:34 +0100

    boo

--- breaking bad/Breaking Bad Full App/app.js
+++ breaking bad/Breaking Bad Full App/app.js
@@ -1,19 +1,14 @@
 const imageDisplay = document.getElementById("img-display");
-const ShowTitle = document.getElementById("showName");
+const characterTitle = document.getElementById("title-name");
 const outputSelect = document.querySelector('#select-output');
 
 const searchTerm = document.querySelector('#input-character-name');
 const outputList = document.getElementById("list-output");
 
+//Character information from The Breaking Bad API
+const characters = `https://www.breakingbadapi.com/api/characters/`;
 
-const Shows = 'https://api.tvmaze/search/shows?q=:query.com';
-
-fetch(show)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-let showName = "";
+let characterName = "";
 
 //Initialise the app with data to produce a select for the characters
 initData();
@@ -29,7 +24,7 @@ function getDetails(){
   //fetch a response from the API, then get the json object from that response 
   //and then examine json object to get relevant information for the app output
   //and if an error, output error
-  fetch(games).then(getJson).then(updateDisplay).catch(reportError);
+  fetch(characters).then(getJson).then(updateDisplay).catch(reportError);
 }
 
 
@@ -43,24 +38,24 @@ function updateDisplay(jsonObj){
 
   
 
-  let showObjArray = jsonObj;       //get json object from response
+  let charObjArray = jsonObj;       //get json object from response
                                     //jsonObj is an array of character objects
 
   //Check to see if user input matches a character
-  for (let ashowObj of showObjArray){   
-    if (ashowObj.title === outputSelect.value){
-      showObj = ashowObj;
+  for (let aCharObj of charObjArray){   
+    if (aCharObj.name === outputSelect.value){
+      charObj = aCharObj;
     }  
   }
   //console.log(charObj);
 
-  let showName = showObj.title;      //Get character name
-  showTitle.textContent = showObj.title; //Set title to be character name
-  let showImageURL = showObj.img;   //Get character image
-  imageDisplay.src= showImageURL;    //Set image to be character image
+  let charName = charObj.name;      //Get character name
+  characterTitle.textContent = charObj.name; //Set title to be character name
+  let charImageURL = charObj.img;   //Get character image
+  imageDisplay.src=charImageURL;    //Set image to be character image
   
   removeAllListItems();             //remove all previous list items
-  makeDetailsList(showObj)          //Create list of character attributes underneath image
+  makeDetailsList(charObj)          //Create list of character attributes underneath image
 }
 
 //-------------------------------------------------------------------
@@ -70,19 +65,19 @@ function reportError(anError){
 }
 //-------------------------------------------------------------------
 
-function getshowNameInput(){
+function getCharacterNameInput(){
   //console.log(searchTerm.value);
   return searchTerm.value;
   //return "Walter White";
 }
 //-------------------------------------------------------------------
 
-function makeDetailsList(ashowObj){
-  let showPropertyList = ["title", "rating", "platform", "released"];
+function makeDetailsList(aCharObj){
+  let characterPropertyList = ["birthday", "nickname", "portrayed", "status"];
   
-  for (let showProperty of showPropertyList){
+  for (let characterProperty of characterPropertyList){
     const newItem = document.createElement('ion-item');
-    let outputText = showProperty.toUpperCase() + ":   " + ashowObj[showProperty];
+    let outputText = characterProperty.toUpperCase() + ":   " + aCharObj[characterProperty];
     newItem.textContent = outputText;
 
     outputList.appendChild(newItem);
@@ -99,35 +94,35 @@ function removeAllListItems(){
 //-------------------------------------------------------------------
 
 function initData(){
-  fetch(shows).then(getJson).then(initSelect).then(reportError);  
+  fetch(characters).then(getJson).then(initSelect).then(reportError);  
 }
 //-------------------------------------------------------------------
 
 function initSelect(jsonObj){
 
-  let showObjectArray = jsonObj.results;
+  let characterObjectArray = jsonObj;
   //console.log(characterObjectArray);
-  let showNamesArray = [];
+  let characterNamesArray = [];
 
-  for (let showObject of showObjectArray){
-    showNamesArray.push(showObject.name);
+  for (let characterObject of characterObjectArray){
+    characterNamesArray.push(characterObject.name);
   }
   //console.log(characterNamesArray);
-  buildSelectOptions(showNamesArray);
+  buildSelectOptions(characterNamesArray);
 }
 //-------------------------------------------------------------------
 
-function buildSelectOptions(anArrayOfshowNames){
+function buildSelectOptions(anArrayOfCharacterNames){
   
-  for (let showName of anArrayOfshowNames){
-    createSelectOption(showName)
+  for (let charName of anArrayOfCharacterNames){
+    createSelectOption(charName)
   }
 }
 //-------------------------------------------------------------------
 function createSelectOption(aName){
   const newItem = document.createElement('ion-select-option');
   newItem.value = aName;
-  newItem.textContent = aName;
+  newItem.textContent = aName.toUpperCase();
 
   outputSelect.appendChild(newItem);  
 }
---
+++ ion pages/javascript.js
@@ -0,0 +1,66 @@
+$(function() {
+  $("#search_button").click(searchShows);
+})
+
+function getList(url) {
+  $.getJSON(url, function(shows) {
+      console.log(shows);
+
+      let show_info = "";
+
+      shows.forEach(function(show_wrapper) {
+          let show = show_wrapper.show;
+          let show_name = "Unknown";
+          let show_avg_rating = "Unknown";
+
+          if (show.hasOwnProperty("name") && show.name !== null) {
+              show_name = show.name;
+          }
+
+          if (show.hasOwnProperty("rating") && show.rating !== null) {
+              if (show.rating.hasOwnProperty("average") && show.rating.average !== null) {
+                  show_avg_rating = show.rating.average;
+              }
+          }
+
+          show_info += "Title: " + show_name + "<br>" +
+          "Average Rating: " + show_avg_rating + "<br><br>";
+      })
+
+      $("#search_found").html(show_info);
+  })
+}
+
+function searchShows() {
+  var url = "https://api.tvmaze.com/search/shows?q=";
+  var search_terms = getSearchTerms();
+
+  if (search_terms !== null) {
+      $("#error_msg").html("")
+      url += search_terms;
+      getList(url);
+  }
+  else {
+      $("#error_msg").html("Please enter search term");
+  }
+}
+
+function getSearchTerms() {
+  let search_term = $("#search_term").val();
+  let search_term_array = search_term.trim().split(/\ +/);
+
+  if (search_term_array.length > 0 && search_term_array[0] !== "") {
+      let final_search_term = search_term_array[0];
+
+      if (search_term_array.length > 1) {
+          for (let i = 1; i < search_term_array.length; i++) {
+              final_search_term += "+" + search_term_array[i];
+          }
+      }
+
+      return final_search_term;
+  }
+  else {
+      return null;
+  }
+}
